8ca345ef6920c3873ce4f4bcdd71325a9caf24ef
quanghung306 <94045688+quanghung306@users.noreply.github.com>
2024-04-16 21:03:13 +0700

    commit

---
+++ src/conteinar/FormContainer.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { Button, Col, Form, FormGroup, Input, Label, Row } from "reactstrap";
+import FormField from "../componets/FormField";
+const FormContainer = () => {
+  const [emailValue, setEmailValue] = useState("");
+  const onEmailValueChange = (e) =>{
+    setEmailValue(e.target.value);
+    console.log("", e.target.value);
+  };
+  const [passwordValue, setPasswordValue] = useState("");
+  const onPasswordlValueChange = (e) => {
+    setPasswordValue(e.target.value);
+    console.log("", e.target.value);
+  };
+  const [addressValue, setAddressValue] = useState("");
+  const onAddresslValueChange = (e) => {
+    setAddressValue(e.target.value);
+    console.log("", e.target.value);
+  };
+  const [phoneValue, setPhoneValue] = useState('');
+  const onPhonelValueChange = (e) => {
+   
+    setPhoneValue(e.target.value)
+    console.log("", e.target.value);
+  };
+  const onSubmit=(e)=>{
+   e.preventDefault();
+    if (isNaN(phoneValue)) {
+        alert()
+       return;
+    };
+    const oj={
+        emailValue,
+        passwordValue,
+        addressValue,
+    }
+    console.log("🚀 ~ onSubmi ~ oj:", oj)
+  };
+  return (
+    <Form>
+      <Row>
+        <Col md={6}>
+          <FormField
+            name="email"
+            placeholder="with a placeholder"
+            Label="Email"
+            type="email"
+            value={emailValue}
+            onChange={onEmailValueChange}
+          />
+        </Col>
+        <Col md={6}>
+          <FormField
+            name="password"
+            placeholder="password placeholder"
+            Label="examplePassword"
+            type="password"
+            value={passwordValue}
+            onChange={onPasswordlValueChange}
+          />
+        </Col>
+      </Row>
+      <FormField
+        name="Address"
+        placeholder="Address placeholder"
+        Label="Address"
+        type="text"
+        value={addressValue}
+        onChange={onAddresslValueChange}
+      />
+      <Col md={6}>
+          <FormField
+            name="PhoneNumber"
+            placeholder="PhoneNumber placeholder"
+            Label="PhoneNumber"
+            type="text"
+            value={phoneValue}
+            onChange={onPhonelValueChange}
+          />
+        </Col>
+
+      <Button onClick={onSubmit}>Sign in</Button>
+    </Form>
+  );
+};
+
+export default FormContainer;
